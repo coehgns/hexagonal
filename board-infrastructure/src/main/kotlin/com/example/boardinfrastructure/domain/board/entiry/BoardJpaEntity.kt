7efd3c4468b2c1a3f74017e166e7d4581a5e007c
도훈 <modooboiroo@gmail.com>
2025-04-15 15:57:36 +0900

    feat: BoardJpaEntity

---
+++ board-infrastructure/src/main/kotlin/com/example/boardinfrastructure/domain/board/entiry/BoardJpaEntity.kt
@@ -0,0 +1,16 @@
+package com.example.boardinfrastructure.domain.board.entiry
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "tbl_board")
+class BoardJpaEntity(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(name = "title", nullable = false)
+    val title: String,
+
+    @Column(name = "content")
+    val content: String? = null
+)
