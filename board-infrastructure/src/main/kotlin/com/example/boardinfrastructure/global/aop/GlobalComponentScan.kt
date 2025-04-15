d0c6892a1166e8a3ed6c3b59feddee0f46169947
도훈 <modooboiroo@gmail.com>
2025-04-15 16:01:25 +0900

    feat: GlobalComponentScan

---
+++ board-infrastructure/src/main/kotlin/com/example/boardinfrastructure/global/aop/GlobalComponentScan.kt
@@ -0,0 +1,19 @@
+package com.example.boardinfrastructure.global.aop
+
+import com.example.boarddomain.global.annotation.UseCase
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.ComponentScan.Filter
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.FilterType
+
+@Configuration
+@ComponentScan(
+    basePackages = ["com.example.boardinfrastructure"],
+    includeFilters = [
+        Filter(
+            type = FilterType.ANNOTATION,
+            classes = [UseCase::class]
+        )
+    ]
+)
+class GlobalComponentScan
