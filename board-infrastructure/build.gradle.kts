219dcf014b2e0dc50725a2509e81a5443a5d59f7
coehgns <modooboiroo@gmail.com>
2025-04-19 19:12:34 +0900

    chore: build.gradle.kts

--- board-infrastructure/build.gradle.kts
+++ board-infrastructure/build.gradle.kts
@@ -22,6 +22,9 @@ repositories {
 dependencies {
     implementation(project(":board-domain"))
 
+    implementation("org.mapstruct:mapstruct:1.5.2.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.2.Final")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
