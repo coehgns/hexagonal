51105b54c2a9530a40a811fbe987067eb17951f4
coehgns <modooboiroo@gmail.com>
2025-04-24 15:51:43 +0900

    chore: project 의존성 추가

--- board-domain/build.gradle.kts
+++ board-domain/build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 }
 
 dependencies {
+    implementation(project(":board-infrastructure"))
     implementation("org.mapstruct:mapstruct:1.5.2.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.2.Final")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
