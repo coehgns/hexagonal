e3e30d6a931e040523219b9d7dc68f0ef83b6598
coehgns <modooboiroo@gmail.com>
2025-04-24 14:26:46 +0900

    chore: BoardJpaRepository

--- board-infrastructure/src/main/kotlin/com/example/boardinfrastructure/domain/board/repository/BoardJpaRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.boardinfrastructure.domain.board.repository
-
-import com.example.boardinfrastructure.domain.board.entiry.BoardJpaEntity
-import org.springframework.data.repository.CrudRepository
-
-interface BoardJpaRepository : CrudRepository<BoardJpaEntity, Long>
