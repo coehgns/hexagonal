3f2e9b6fdcad7756ea4e00b69aed91cd57c035b5
coehgns <modooboiroo@gmail.com>
2025-04-24 15:54:21 +0900

    chore: project 의존성 제거

--- board-domain/build.gradle.kts
+++ board-domain/build.gradle.kts
@@ -20,7 +20,6 @@ repositories {
 }
 
 dependencies {
-    implementation(project(":board-infrastructure"))
     implementation("org.mapstruct:mapstruct:1.5.2.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.2.Final")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
