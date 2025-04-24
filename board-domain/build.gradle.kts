bf634086eb64d5193c3fe0f20856eafbe46f8864
coehgns <modooboiroo@gmail.com>
2025-04-24 14:26:11 +0900

    chore: build.gradle.kts에 mapstruct 추가

--- board-domain/build.gradle.kts
+++ board-domain/build.gradle.kts
@@ -20,6 +20,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.mapstruct:mapstruct:1.5.2.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.2.Final")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
