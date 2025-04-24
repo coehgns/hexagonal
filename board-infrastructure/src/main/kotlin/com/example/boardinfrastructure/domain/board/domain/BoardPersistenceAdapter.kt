8e7487ff8437baca36ec39d851ed0544d2d7dc82
coehgns <modooboiroo@gmail.com>
2025-04-24 14:29:41 +0900

    feat: BoardPersistenceAdapter

---
+++ board-infrastructure/src/main/kotlin/com/example/boardinfrastructure/domain/board/domain/BoardPersistenceAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.boardinfrastructure.domain.board.domain
+
+import com.example.boarddomain.domain.board.spi.BoardPort
+import com.example.boarddomain.domain.model.Board
+import com.example.boardinfrastructure.domain.board.domain.mapper.BoardMapper
+import com.example.boardinfrastructure.domain.board.domain.repository.BoardJpaRepository
+import org.springframework.stereotype.Component
+
+@Component
+class BoardPersistenceAdapter(
+    private val boardJpaRepository: BoardJpaRepository,
+    private val boardMapper: BoardMapper
+) : BoardPort {
+    override fun save(board: Board) {
+        boardJpaRepository.save(
+            boardMapper.toEntity(board)
+        )
+    }
+}
