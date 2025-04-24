0f527d1e40bbe6b7924658f7c4939da457b980fa
coehgns <modooboiroo@gmail.com>
2025-04-24 14:28:35 +0900

    feat: BoardMapper

---
+++ board-infrastructure/src/main/kotlin/com/example/boardinfrastructure/domain/board/domain/mapper/BoardMapper.kt
@@ -0,0 +1,11 @@
+package com.example.boardinfrastructure.domain.board.domain.mapper
+
+import com.example.boarddomain.domain.model.Board
+import com.example.boardinfrastructure.domain.board.domain.entiry.BoardJpaEntity
+import com.example.boardinfrastructure.global.mapper.GenericMapper
+import org.mapstruct.Mapper
+
+@Mapper
+abstract class BoardMapper : GenericMapper<BoardJpaEntity, Board> {
+    abstract override fun toEntity(model: Board): BoardJpaEntity
+}
