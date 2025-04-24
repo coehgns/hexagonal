db43c15f98c9520a66d76e77e05bee88c904ed82
coehgns <modooboiroo@gmail.com>
2025-04-24 21:30:12 +0900

    chore: mapstruct 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,8 @@ subprojects {
 }
 
 dependencies {
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
