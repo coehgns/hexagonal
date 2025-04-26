28f3821533be588c089178bfae341c9ed2f916fe
coehgns <modooboiroo@gmail.com>
2025-04-26 23:33:15 +0900

    feat: findBoard 메서드 추가

--- src/main/kotlin/com/example/hexagonal/application/board/port/in/BoardUseCase.kt
+++ src/main/kotlin/com/example/hexagonal/application/board/port/in/BoardUseCase.kt
@@ -1,7 +1,10 @@
 package com.example.hexagonal.application.board.port.`in`
 
 import com.example.hexagonal.adapter.`in`.user.dto.request.CreateBoardRequest
+import com.example.hexagonal.domain.board.model.Board
+import java.util.UUID
 
 interface BoardUseCase {
     fun createBoard(request: CreateBoardRequest)
+    fun findBoard(boardId: UUID): Board
 }
