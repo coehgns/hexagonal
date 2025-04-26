0a232987c4c23cf9f247dfdbbfd640be6fed5539
coehgns <modooboiroo@gmail.com>
2025-04-26 23:32:14 +0900

    refactor: findBoard 메서드 추가

--- src/main/kotlin/com/example/hexagonal/application/board/service/CreateBoardService.kt
+++ src/main/kotlin/com/example/hexagonal/application/board/service/CreateBoardService.kt
@@ -2,13 +2,16 @@ package com.example.hexagonal.application.board.service
 
 import com.example.hexagonal.adapter.`in`.user.dto.request.CreateBoardRequest
 import com.example.hexagonal.application.board.port.`in`.BoardUseCase
+import com.example.hexagonal.application.board.port.out.FindBoardPort
 import com.example.hexagonal.application.board.port.out.SaveBoardPort
 import com.example.hexagonal.domain.board.model.Board
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class CreateBoardService(
-    private val saveBoardPort: SaveBoardPort
+    private val saveBoardPort: SaveBoardPort,
+    private val findBoard: FindBoardPort
 ): BoardUseCase {
     override fun createBoard(request: CreateBoardRequest) {
         val board = request.run {
@@ -19,4 +22,12 @@ class CreateBoardService(
         }
         saveBoardPort.saveBoard(board)
     }
+
+    override fun findBoard(boardId: UUID): Board {
+        val board = findBoard.findById(boardId)
+            ?: throw IllegalArgumentException("board not found.")
+
+        return board
+    }
+
 }
