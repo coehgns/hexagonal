4e0b14f553a4dab036c96d62ef7320c13d7613f3
coehgns <modooboiroo@gmail.com>
2025-04-27 22:09:32 +0900

    feat: BoardMapper에 toModel 추가

--- src/main/kotlin/com/example/hexagonal/adapter/out/persistence/board/BoardMapper.kt
+++ src/main/kotlin/com/example/hexagonal/adapter/out/persistence/board/BoardMapper.kt
@@ -7,4 +7,5 @@ import org.mapstruct.Mapper
 @Mapper(componentModel = "spring")
 interface BoardMapper {
     fun toEntity(board: Board): BoardJpaEntity
+    fun toModel(entity: BoardJpaEntity): Board
 }
