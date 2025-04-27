8b563c912a8d479ec2f985390d1b80ab08865d05
coehgns <modooboiroo@gmail.com>
2025-04-27 22:26:36 +0900

    feat: BoardWebAdapter에 findBoard 메서드 추가

--- src/main/kotlin/com/example/hexagonal/adapter/in/user/BoardWebAdapter.kt
+++ src/main/kotlin/com/example/hexagonal/adapter/in/user/BoardWebAdapter.kt
@@ -2,10 +2,14 @@ package com.example.hexagonal.adapter.`in`.user
 
 import com.example.hexagonal.adapter.`in`.user.dto.request.CreateBoardRequest
 import com.example.hexagonal.application.board.port.`in`.BoardUseCase
+import com.example.hexagonal.domain.board.model.Board
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/boards")
@@ -14,4 +18,8 @@ class BoardWebAdapter(
 ) {
     @PostMapping
     fun createBoard(@RequestBody request: CreateBoardRequest) = boardUseCase.createBoard(request)
+
+    @GetMapping("/{boardId}")
+    fun findBoard(@PathVariable("boardId") boardId: UUID): Board = boardUseCase.findBoard(boardId)
+
 }
