ea753e89a2312cfece572de8562cfae880be05df
coehgns <modooboiroo@gmail.com>
2025-04-28 11:25:39 +0900

    feat: GetBoardListService

---
+++ src/main/kotlin/com/example/hexagonal/application/board/service/GetBoardListService.kt
@@ -0,0 +1,25 @@
+package com.example.hexagonal.application.board.service
+
+import com.example.hexagonal.adapter.`in`.user.dto.response.GetBoardResponse
+import com.example.hexagonal.application.board.port.`in`.GetBoardListUseCase
+import com.example.hexagonal.application.board.port.out.BoardPort
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class GetBoardListService(
+    private val boardPort: BoardPort
+) : GetBoardListUseCase {
+    @Transactional(readOnly = true)
+    override fun getBoardList(): List<GetBoardResponse> {
+        val boards = boardPort.findAll()
+
+        return boards.map {
+            GetBoardResponse(
+                boardId = it.id!!,
+                title = it.title,
+                content = it.content
+            )
+        }
+    }
+}
