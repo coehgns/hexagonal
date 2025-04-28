e64aef2164e4fa99efae62c7b8f1c899bdaafa00
coehgns <modooboiroo@gmail.com>
2025-04-28 12:08:30 +0900

    feat: ModifyBoardService 예외 추가

--- src/main/kotlin/com/example/hexagonal/application/board/service/ModifyBoardService.kt
+++ src/main/kotlin/com/example/hexagonal/application/board/service/ModifyBoardService.kt
@@ -3,6 +3,7 @@ package com.example.hexagonal.application.board.service
 import com.example.hexagonal.adapter.`in`.user.dto.request.ModifyBoardRequest
 import com.example.hexagonal.application.board.port.`in`.ModifyBoardUseCase
 import com.example.hexagonal.application.board.port.out.BoardPort
+import com.example.hexagonal.domain.board.exception.BoardNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
@@ -13,9 +14,7 @@ class ModifyBoardService(
 ) : ModifyBoardUseCase {
     @Transactional
     override fun modifyBoard(boardId: UUID, request: ModifyBoardRequest) {
-        val board = boardPort.findById(boardId)
-            ?: throw IllegalArgumentException("board not found.")
-
+        val board = boardPort.findById(boardId) ?: throw BoardNotFoundException
         boardPort.saveBoard(board.modifyBoard(request))
     }
 }
