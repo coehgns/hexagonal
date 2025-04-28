d5f34b47e56bca00f9364b4028a68365b9aeb770
coehgns <modooboiroo@gmail.com>
2025-04-28 14:08:59 +0900

    feat: UserNotFoundException

--- src/main/kotlin/com/example/hexagonal/domain/user/exception/UserNotFoundException.kt
+++ src/main/kotlin/com/example/hexagonal/domain/user/exception/UserNotFoundException.kt
@@ -3,4 +3,4 @@ package com.example.hexagonal.domain.user.exception
 import com.example.hexagonal.global.error.exception.CustomException
 import com.example.hexagonal.global.error.exception.ErrorCode
 
-object UserNotFoundException : CustomException(ErrorCode.USER_NOT_FOUND
+object UserNotFoundException : CustomException(ErrorCode.USER_NOT_FOUND)
