eb991107af220745de33746664738e3f7a698194
coehgns <modooboiroo@gmail.com>
2025-04-28 14:16:51 +0900

    feat: FindUserPort

---
+++ src/main/kotlin/com/example/hexagonal/application/user/port/out/FindUserPort.kt
@@ -0,0 +1,8 @@
+package com.example.hexagonal.application.user.port.out
+
+import com.example.hexagonal.domain.user.model.User
+import java.util.UUID
+
+interface FindUserPort {
+    fun findById(userId: UUID): User?
+}
