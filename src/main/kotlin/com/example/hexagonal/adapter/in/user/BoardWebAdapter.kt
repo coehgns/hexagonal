e84d990881210ffe2294a9407f4948ee4b51f81b
coehgns <modooboiroo@gmail.com>
2025-04-28 09:23:37 +0900

    chore: import 삭제

--- src/main/kotlin/com/example/hexagonal/adapter/in/user/BoardWebAdapter.kt
+++ src/main/kotlin/com/example/hexagonal/adapter/in/user/BoardWebAdapter.kt
@@ -3,7 +3,6 @@ package com.example.hexagonal.adapter.`in`.user
 import com.example.hexagonal.adapter.`in`.user.dto.request.CreateBoardRequest
 import com.example.hexagonal.adapter.`in`.user.dto.response.GetBoardResponse
 import com.example.hexagonal.application.board.port.`in`.BoardUseCase
-import com.example.hexagonal.domain.board.model.Board
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
