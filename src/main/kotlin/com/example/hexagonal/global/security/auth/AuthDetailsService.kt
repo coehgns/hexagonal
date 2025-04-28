ce83e159c0cd5d3236cf961c5f39abfd46184b50
coehgns <modooboiroo@gmail.com>
2025-04-28 14:18:24 +0900

    refactor: AuthDetailsService

--- src/main/kotlin/com/example/hexagonal/global/security/auth/AuthDetailsService.kt
+++ src/main/kotlin/com/example/hexagonal/global/security/auth/AuthDetailsService.kt
@@ -1,6 +1,6 @@
 package com.example.hexagonal.global.security.auth
 
-import com.example.hexagonal.application.user.port.out.FindUserPort
+import com.example.hexagonal.application.user.port.out.UserPort
 import com.example.hexagonal.domain.user.exception.UserNotFoundException
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -9,10 +9,10 @@ import java.util.UUID
 
 @Component
 class AuthDetailsService(
-    private val findUserPort: FindUserPort
+    private val userPort: UserPort
 ) : UserDetailsService {
     override fun loadUserByUsername(userId: String?): UserDetails {
-        val user = findUserPort.findById(UUID.fromString(userId)) ?: throw UserNotFoundException
+        val user = userPort.findById(UUID.fromString(userId)) ?: throw UserNotFoundException
         return AuthDetails(user)
     }
 }
