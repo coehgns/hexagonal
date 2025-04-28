fa8200a4dd7fea647f0c42c0ffc88138af7edfcf
coehgns <modooboiroo@gmail.com>
2025-04-28 21:46:14 +0900

    feat: Board model에 @Aggregate 추가

--- src/main/kotlin/com/example/hexagonal/domain/board/model/Board.kt
+++ src/main/kotlin/com/example/hexagonal/domain/board/model/Board.kt
@@ -1,8 +1,10 @@
 package com.example.hexagonal.domain.board.model
 
 import com.example.hexagonal.adapter.`in`.board.dto.request.ModifyBoardRequest
+import com.example.hexagonal.global.annotation.Aggregate
 import java.util.*
 
+@Aggregate
 data class Board(
     val id: UUID? = null,
     val title: String,
