378b6d50cd8484c686bd0cb5fab8c99b29c791ba
coehgns <modooboiroo@gmail.com>
2025-04-28 19:04:33 +0900

    feat: ErrorCode에 USER_ALREADY_EXISTS 추가

--- src/main/kotlin/com/example/hexagonal/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/com/example/hexagonal/global/error/exception/ErrorCode.kt
@@ -5,5 +5,6 @@ enum class ErrorCode(
     val message: String
 ) {
     BOARD_NOT_FOUND(404, "Board Not Found."),
-    USER_NOT_FOUND(404, "User Not Found.")
+    USER_NOT_FOUND(404, "User Not Found."),
+    USER_ALREADY_EXISTS(409, "User Already Exist.")
 }
