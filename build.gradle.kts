318fcbcf2efc930f03c567aa15e0f4a18773fa6b
coehgns <modooboiroo@gmail.com>
2025-04-28 16:21:22 +0900

    chore: build.gradle.kts에 jwt 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,9 @@ subprojects {
 }
 
 dependencies {
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5") // Jackson 기반 파서
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.security:spring-security-config")
