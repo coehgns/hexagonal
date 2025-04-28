a435fdd49ebc9d14d47261ee90d82f6c501d4a38
coehgns <modooboiroo@gmail.com>
2025-04-28 14:16:46 +0900

    feat: UserJpaRepository

---
+++ src/main/kotlin/com/example/hexagonal/adapter/out/persistence/user/repository/UserJpaRepository.kt
@@ -0,0 +1,7 @@
+package com.example.hexagonal.adapter.out.persistence.user.repository
+
+import com.example.hexagonal.infrastructure.persistence.user.UserJpaEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface UserJpaRepository : JpaRepository<UserJpaEntity, UUID>
