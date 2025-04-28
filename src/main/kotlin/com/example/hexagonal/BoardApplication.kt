c6a44bf650f04a12ca050dce143ae502a6c0a59e
coehgns <modooboiroo@gmail.com>
2025-04-28 16:49:51 +0900

    feat: BoardApplication에 @ConfigurationPropertiesScan 추가

--- src/main/kotlin/com/example/hexagonal/BoardApplication.kt
+++ src/main/kotlin/com/example/hexagonal/BoardApplication.kt
@@ -1,11 +1,13 @@
 package com.example.hexagonal
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@ConfigurationPropertiesScan
 class BoardApplicatoin
 
 fun main(args: Array<String>) {
