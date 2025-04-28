6f82b4e15b8e1585c2c35e5c4eabec158c8ff193
coehgns <modooboiroo@gmail.com>
2025-04-28 09:21:52 +0900

    refactor: BoardUseCase

--- src/main/kotlin/com/example/hexagonal/application/board/port/in/BoardUseCase.kt
+++ src/main/kotlin/com/example/hexagonal/application/board/port/in/BoardUseCase.kt
@@ -2,12 +2,12 @@ package com.example.hexagonal.application.board.port.`in`
 
 import com.example.hexagonal.adapter.`in`.user.dto.request.CreateBoardRequest
 import com.example.hexagonal.adapter.`in`.user.dto.request.ModifyBoardRequest
-import com.example.hexagonal.domain.board.model.Board
+import com.example.hexagonal.adapter.`in`.user.dto.response.GetBoardResponse
 import java.util.UUID
 
 interface BoardUseCase {
     fun createBoard(request: CreateBoardRequest)
-    fun findBoard(boardId: UUID): Board
-    fun findAllBoard(): List<Board>
+    fun findBoard(boardId: UUID): GetBoardResponse
+    fun findAllBoard(): List<GetBoardResponse>
     fun modifyBoard(request: ModifyBoardRequest)
 }
