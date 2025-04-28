a1fb99a0862fd672538071d5321c41776ed95f0b
coehgns <modooboiroo@gmail.com>
2025-04-28 19:26:20 +0900

    feat: SecurityConfig에 @Configuration 추가

--- src/main/kotlin/com/example/hexagonal/global/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/hexagonal/global/config/security/SecurityConfig.kt
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsUtils
 
+@Configuration
 @EnableWebSecurity
 class SecurityConfig() {
     @Bean
