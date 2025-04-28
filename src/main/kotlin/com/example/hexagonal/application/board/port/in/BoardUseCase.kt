f0e0e060b1b78c0127164e7a00e81309c8f197af
coehgns <modooboiroo@gmail.com>
2025-04-28 09:12:42 +0900

    feat: BoardUseCase에 modifyBoard 메서드 추가

--- src/main/kotlin/com/example/hexagonal/application/board/port/in/BoardUseCase.kt
+++ src/main/kotlin/com/example/hexagonal/application/board/port/in/BoardUseCase.kt
@@ -1,6 +1,7 @@
 package com.example.hexagonal.application.board.port.`in`
 
 import com.example.hexagonal.adapter.`in`.user.dto.request.CreateBoardRequest
+import com.example.hexagonal.adapter.`in`.user.dto.request.ModifyBoardRequest
 import com.example.hexagonal.domain.board.model.Board
 import java.util.UUID
 
@@ -8,4 +9,5 @@ interface BoardUseCase {
     fun createBoard(request: CreateBoardRequest)
     fun findBoard(boardId: UUID): Board
     fun findAllBoard(): List<Board>
+    fun modifyBoard(request: ModifyBoardRequest)
 }
