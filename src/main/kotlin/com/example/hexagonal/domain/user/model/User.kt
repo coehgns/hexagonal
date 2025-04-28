31790d9a85b13684ae115f0aaea726046b01f1ad
coehgns <modooboiroo@gmail.com>
2025-04-28 21:46:09 +0900

    feat: User model에 @Aggregate 추가

--- src/main/kotlin/com/example/hexagonal/domain/user/model/User.kt
+++ src/main/kotlin/com/example/hexagonal/domain/user/model/User.kt
@@ -1,7 +1,9 @@
 package com.example.hexagonal.domain.user.model
 
+import com.example.hexagonal.global.annotation.Aggregate
 import java.util.UUID
 
+@Aggregate
 data class User(
     val id: UUID?,
     val userName: String,
