870d8ffd6f8e6ae7e6c1b50c8511b962de787c25
coehgns <modooboiroo@gmail.com>
2025-04-28 11:25:15 +0900

    feat: CreateBoardService

---
+++ src/main/kotlin/com/example/hexagonal/application/board/service/CreateBoardService.kt
@@ -0,0 +1,24 @@
+package com.example.hexagonal.application.board.service
+
+import com.example.hexagonal.adapter.`in`.user.dto.request.CreateBoardRequest
+import com.example.hexagonal.application.board.port.`in`.CreateBoardUseCase
+import com.example.hexagonal.application.board.port.out.BoardPort
+import com.example.hexagonal.domain.board.model.Board
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class CreateBoardService(
+    private val boardPort: BoardPort
+) : CreateBoardUseCase{
+    @Transactional
+    override fun createBoard(request: CreateBoardRequest) {
+        val board = request.run {
+            Board(
+                title = title,
+                content = content
+            )
+        }
+        boardPort.saveBoard(board)
+    }
+}
