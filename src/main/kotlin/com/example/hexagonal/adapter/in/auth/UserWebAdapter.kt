06bfae5ccf30f39d698878d7b081b58be898a6bd
coehgns <modooboiroo@gmail.com>
2025-04-28 19:41:17 +0900

    feat: UserWebAdapter에 login 메서드 추가

--- src/main/kotlin/com/example/hexagonal/adapter/in/auth/UserWebAdapter.kt
+++ src/main/kotlin/com/example/hexagonal/adapter/in/auth/UserWebAdapter.kt
@@ -1,7 +1,10 @@
 package com.example.hexagonal.adapter.`in`.auth
 
+import com.example.hexagonal.adapter.`in`.auth.dto.request.LoginRequest
 import com.example.hexagonal.adapter.`in`.auth.dto.request.SignupRequest
+import com.example.hexagonal.application.auth.port.`in`.LoginUseCase
 import com.example.hexagonal.application.auth.port.`in`.SignupUseCase
+import com.example.hexagonal.global.security.jwt.dto.TokenResponse
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/auth")
 class UserWebAdapter(
-    private val signupUseCase: SignupUseCase
+    private val signupUseCase: SignupUseCase,
+    private val loginUseCase: LoginUseCase
 ) {
     @PostMapping("/signup")
     fun signup(@RequestBody request: SignupRequest) = signupUseCase.signup(request)
+
+    @PostMapping("/login")
+    fun login(@RequestBody request: LoginRequest): TokenResponse =
+        loginUseCase.login(request)
 }
