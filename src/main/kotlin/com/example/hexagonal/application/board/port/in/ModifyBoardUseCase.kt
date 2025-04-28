e3d8e597373885fb35180b5a574f46cc01fa2f01
coehgns <modooboiroo@gmail.com>
2025-04-28 11:26:02 +0900

    feat: ModifyBoardUseCase

---
+++ src/main/kotlin/com/example/hexagonal/application/board/port/in/ModifyBoardUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.hexagonal.application.board.port.`in`
+
+import com.example.hexagonal.adapter.`in`.user.dto.request.ModifyBoardRequest
+import java.util.*
+
+interface ModifyBoardUseCase {
+    fun modifyBoard(boardId: UUID, request: ModifyBoardRequest)
+}
