1979d1e1fd9899167f1a1d7b4cb28fbed75e3b73
coehgns <modooboiroo@gmail.com>
2025-04-28 12:08:13 +0900

    feat: DeleteBoardService에 예외 추가

--- src/main/kotlin/com/example/hexagonal/application/board/service/DeleteBoardService.kt
+++ src/main/kotlin/com/example/hexagonal/application/board/service/DeleteBoardService.kt
@@ -2,6 +2,7 @@ package com.example.hexagonal.application.board.service
 
 import com.example.hexagonal.application.board.port.`in`.DeleteBoardUseCase
 import com.example.hexagonal.application.board.port.out.BoardPort
+import com.example.hexagonal.domain.board.exception.BoardNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
@@ -12,9 +13,7 @@ class DeleteBoardService(
 ) : DeleteBoardUseCase{
     @Transactional
     override fun deleteBoard(boardId: UUID) {
-        val board = boardPort.findById(boardId)
-            ?: throw IllegalArgumentException("board not found.")
-
+        val board = boardPort.findById(boardId) ?: throw BoardNotFoundException
         boardPort.deleteBoard(board)
     }
 }
