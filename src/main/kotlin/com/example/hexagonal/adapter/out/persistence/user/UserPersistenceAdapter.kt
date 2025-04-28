afc8df67ce6d787dbff9e2f1e6ac338af16cf5a8
coehgns <modooboiroo@gmail.com>
2025-04-28 14:17:20 +0900

    feat: UserPersistenceAdapter

---
+++ src/main/kotlin/com/example/hexagonal/adapter/out/persistence/user/UserPersistenceAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.hexagonal.adapter.out.persistence.user
+
+import com.example.hexagonal.adapter.out.persistence.user.repository.UserJpaRepository
+import com.example.hexagonal.application.user.port.out.FindUserPort
+import com.example.hexagonal.domain.user.model.User
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class UserPersistenceAdapter(
+    private val userJpaRepository: UserJpaRepository,
+    private val userMapper: UserMapper
+) : FindUserPort {
+    override fun findById(userId: UUID): User? {
+        return userJpaRepository.findByIdOrNull(userId)?.let { userMapper.toModel(it) }
+    }
+}
