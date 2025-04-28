58dd8ffd7b43a3027985f0fbd0f202fb96065649
coehgns <modooboiroo@gmail.com>
2025-04-28 19:07:31 +0900

    feat: SignupService

---
+++ src/main/kotlin/com/example/hexagonal/application/auth/port/service/SignupService.kt
@@ -0,0 +1,31 @@
+package com.example.hexagonal.application.auth.port.service
+
+import com.example.hexagonal.adapter.`in`.auth.dto.request.SignupRequest
+import com.example.hexagonal.application.auth.port.`in`.SignupUseCase
+import com.example.hexagonal.application.user.port.out.ExistUserPort
+import com.example.hexagonal.application.user.port.out.UserPort
+import com.example.hexagonal.domain.user.exception.UserAlreadyExistsException
+import com.example.hexagonal.domain.user.model.User
+import org.springframework.stereotype.Service
+
+@Service
+class SignupService(
+    private val userPort: UserPort,
+    private val existUserPort: ExistUserPort
+) : SignupUseCase {
+    override fun signup(signupRequest: SignupRequest) {
+        val user = signupRequest.run {
+            User(
+                id = null,
+                userName = userName,
+                password = password
+            )
+        }
+
+        if (existUserPort.existsByUserName(user.userName)) {
+            throw UserAlreadyExistsException
+        }
+
+        userPort.saveUser(user)
+    }
+}
