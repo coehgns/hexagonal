b6700afdb43f613e29b5815e8abf95b8545f9be3
coehgns <modooboiroo@gmail.com>
2025-04-28 19:26:28 +0900

    feat: SignupService

--- src/main/kotlin/com/example/hexagonal/application/auth/port/service/SignupService.kt
+++ src/main/kotlin/com/example/hexagonal/application/auth/port/service/SignupService.kt
@@ -6,19 +6,21 @@ import com.example.hexagonal.application.user.port.out.ExistUserPort
 import com.example.hexagonal.application.user.port.out.UserPort
 import com.example.hexagonal.domain.user.exception.UserAlreadyExistsException
 import com.example.hexagonal.domain.user.model.User
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class SignupService(
     private val userPort: UserPort,
-    private val existUserPort: ExistUserPort
+    private val existUserPort: ExistUserPort,
+    private val passwordEncoder: PasswordEncoder
 ) : SignupUseCase {
     override fun signup(signupRequest: SignupRequest) {
         val user = signupRequest.run {
             User(
                 id = null,
                 userName = userName,
-                password = password
+                password = passwordEncoder.encode(signupRequest.password)
             )
         }
 
