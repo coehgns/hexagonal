855a847c098cc814c3c95da6bba041d836c19817
coehgns <modooboiroo@gmail.com>
2025-04-28 09:19:28 +0900

    feat: boardId 필드 추가

--- src/main/kotlin/com/example/hexagonal/adapter/in/user/dto/response/GetBoardResponse.kt
+++ src/main/kotlin/com/example/hexagonal/adapter/in/user/dto/response/GetBoardResponse.kt
@@ -1,6 +1,9 @@
 package com.example.hexagonal.adapter.`in`.user.dto.response
 
+import java.util.*
+
 data class GetBoardResponse(
+    val boardId: UUID,
     val title: String,
     val content: String?
 )
