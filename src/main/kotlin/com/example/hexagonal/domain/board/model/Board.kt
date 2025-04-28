af177202b542d2ddfa54261f5f7664c46f607462
coehgns <modooboiroo@gmail.com>
2025-04-28 11:04:57 +0900

    feat: modifyBoard 메서드 추가

--- src/main/kotlin/com/example/hexagonal/domain/board/model/Board.kt
+++ src/main/kotlin/com/example/hexagonal/domain/board/model/Board.kt
@@ -1,9 +1,14 @@
 package com.example.hexagonal.domain.board.model
 
+import com.example.hexagonal.adapter.`in`.user.dto.request.ModifyBoardRequest
 import java.util.*
 
 data class Board(
     val id: UUID? = null,
     val title: String,
     val content: String? = null
-)
+) {
+    fun modifyBoard(request: ModifyBoardRequest): Board {
+        return copy(title = request.title, content = request.content)
+    }
+}
