4d0d29a94c4aca62b4193b51670f1dd655e507ae
coehgns <modooboiroo@gmail.com>
2025-04-28 19:30:19 +0900

    feat: LoginUseCase에 반환값 추가

--- src/main/kotlin/com/example/hexagonal/application/auth/port/in/LoginUseCase.kt
+++ src/main/kotlin/com/example/hexagonal/application/auth/port/in/LoginUseCase.kt
@@ -1,7 +1,8 @@
 package com.example.hexagonal.application.auth.port.`in`
 
 import com.example.hexagonal.adapter.`in`.auth.dto.request.LoginRequest
+import com.example.hexagonal.global.security.jwt.dto.TokenResponse
 
 interface LoginUseCase {
-    fun login(request: LoginRequest)
+    fun login(request: LoginRequest): TokenResponse
 }
