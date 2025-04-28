87500be8617d9718a2056d1767c3f5ee231cfe03
coehgns <modooboiroo@gmail.com>
2025-04-28 12:08:25 +0900

    feat: GetBoardService 예외 추가

--- src/main/kotlin/com/example/hexagonal/application/board/service/GetBoardService.kt
+++ src/main/kotlin/com/example/hexagonal/application/board/service/GetBoardService.kt
@@ -3,6 +3,7 @@ package com.example.hexagonal.application.board.service
 import com.example.hexagonal.adapter.`in`.user.dto.response.GetBoardResponse
 import com.example.hexagonal.application.board.port.`in`.GetBoardUseCase
 import com.example.hexagonal.application.board.port.out.BoardPort
+import com.example.hexagonal.domain.board.exception.BoardNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
@@ -13,9 +14,7 @@ class GetBoardService(
 ) : GetBoardUseCase {
     @Transactional(readOnly = true)
     override fun getBoard(boardId: UUID): GetBoardResponse {
-        val board = boardPort.findById(boardId)
-            ?: throw IllegalArgumentException("board not found.")
-
+        val board = boardPort.findById(boardId) ?: throw BoardNotFoundException
         return GetBoardResponse(
             boardId = board.id!!,
             title = board.title,
